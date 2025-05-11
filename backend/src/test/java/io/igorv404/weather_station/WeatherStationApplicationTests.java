c696357ebb144e27b7ee7966e77ed8e385fd2ba5
Igor Vilkov <igorvilkov404@gmail.com>
2025-05-11 15:39:43 +0300

    initial commit for backend

---
+++ backend/src/test/java/io/igorv404/weather_station/WeatherStationApplicationTests.java
@@ -0,0 +1,11 @@
+package io.igorv404.weather_station;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherStationApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
